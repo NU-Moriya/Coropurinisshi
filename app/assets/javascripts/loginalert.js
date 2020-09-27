8fa6622f6a5a28c01b68007d57d81a322e525f95
nobuko.m <animopara100pre@gmail.com>
2020-09-27 23:17:31 +0900

    Add Js_window_alert

---
+++ app/assets/javascripts/loginalert.js
@@ -0,0 +1,9 @@
+const hour = new Date().getHours();
+  
+if(hour >= 19) {
+  window.alert('こんばんは。今日も1日お疲れ様でした♡');
+} else {
+  window.alert('どうも！こころぷりぷり行きましょう！');
+}
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+    
 
   def index
     @users = User.all
@@ -34,4 +35,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction )
   end
+
+  
 end
